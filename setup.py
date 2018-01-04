7f89a97c5e367b8414ae3a0f2fb856b358160557
Swall0w <technext.jpn@gmail.com>
2018-01-04 21:44:13 +0900

    [update]: fix.

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     version="0.1.0",
     description="Create Issues about arxiv papers",
     url="https://github.com/Swall0w/arxiv-bot",
-    ;x
     license=license,
     packages=find_packages(exclude=('tests')),
     test_suite='tests',
